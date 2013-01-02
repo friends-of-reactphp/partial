1d433eedb62fea82d77bbe42ba503c7a2e79088b
Igor Wiedler <igor@wiedler.ch>
2013-01-02 22:47:57 +0100

    Proposed function-based API for upcoming 1.1.0

--- examples/curry.php
+++ examples/curry.php
@@ -2,13 +2,13 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-use React\Curry\Util as Curry;
+use React\Curry;
 
 $add = function ($a, $b) {
     return $a + $b;
 };
 
-$addOne = Curry::bind($add, 1);
+$addOne = Curry\bind($add, 1);
 
 echo sprintf("%s\n", $addOne(5));
 // outputs 6
